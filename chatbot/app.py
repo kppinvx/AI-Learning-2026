f5eb557c1e3a793007e1e83e8f2b7acda9209094
Ktul Patel <ketul.patel@innvonix.com>
2026-08-07 18:15:41 +0530

    python code with gemini chatbot

---
+++ chatbot/app.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-flash-latest",
+    temperature=1
+)
+
+print("Gemini Chatbot")
+print("Type 'exit' to quit\n")
+
+while True:
+    question = input("You: ")
+
+    if question.lower() == "exit":
+        break
+
+    response = llm.invoke([
+        HumanMessage(content=question)
+    ])
+
+    if isinstance(response.content, list):
+        text = "".join(
+            item.get("text", "")
+            for item in response.content
+            if item.get("type") == "text"
+        )
+    else:
+        text = response.content
+
+    print(f"\nBot: {text}\n")
+    print()
